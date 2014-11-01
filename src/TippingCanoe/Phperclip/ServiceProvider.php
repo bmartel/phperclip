6481bea6ff7d1a146bc19380d6802453c2416c75
bmartel <brandonmartel@gmail.com>
2014-11-01 16:17:41 -0500

    some fixes in the service provider bindings

--- src/TippingCanoe/Phperclip/ServiceProvider.php
+++ src/TippingCanoe/Phperclip/ServiceProvider.php
@@ -6,7 +6,8 @@ namespace TippingCanoe\Phperclip;
 use TippingCanoe\Phperclip\Processes\ProcessManager;
 use TippingCanoe\Phperclip\Service as PhperclipService;
 
-class ServiceProvider extends \Illuminate\Support\ServiceProvider{
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
 
 	/**
 	 * Indicates if loading of the provider is deferred.
@@ -20,8 +21,12 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider{
 	 *
 	 * @return void
 	 */
-	public function boot() {
+	public function boot()
+	{
 		$this->package('tippingcanoe/phperclip');
+
+		// Register the filename generator ahead of the other registrations
+		$this->registerFileNameGenerator();
 	}
 
 	/**
@@ -29,52 +34,25 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider{
 	 *
 	 * @return void
 	 */
-	public function register() {
-
+	public function register()
+	{
 		/** @var \Illuminate\Config\Repository $config */
 		$config = $this->app->make('config');
-
-		$this->app->bind('TippingCanoe\Phperclip\Contracts\FileNameGenerator', $config->get('phperclip::filename_generator'));
 
 		$this->app->bind('TippingCanoe\Phperclip\Repository\FileInterface', 'TippingCanoe\Phperclip\Repository\File');
 
 		$this->app->bindShared('TippingCanoe\Phperclip\Service', function ($app) use ($config) {
 
-			//
-			// Amazon S3
-			//
-			if($s3Config = $config->get('phperclip::s3')) {
-				$this->app->bind('Aws\S3\S3Client', function ($app) use ($s3Config) {
-					return \Aws\S3\S3Client::factory($s3Config);
-				});
-			}
+			// Register the amazon aws client
+			$this->registerAwsClient($config);
 
-			// Run through and call each config option as a setter on the storage method.
-			$storageDrivers = [];
-			foreach($config->get('phperclip::storage', []) as $abstract => $driverConfig) {
-
-				$driver = $app->make($abstract);
-
-				foreach($driverConfig as $property => $value) {
-					$setter = studly_case('set_' . $property);
-					$driver->$setter($value);
-				}
-
-				$storageDrivers[$abstract] = $driver;
-
-			}
+			// Configure the storage drivers
+			$storageDrivers = $this->configureStorageDrivers($app, $config);
 
 			// Register File processors
-			$processors = [];
-			if($processorConfig = $config->get('phperclip::processors')){
+			$this->registerFileProcessors($app, $config);
 
-				foreach($processorConfig as $processor) {
-					$processors[] = $app->make($processor);
-				}
-
-				$this->app->bindShared('TippingCanoe\Phperclip\Processes\ProcessManager', new ProcessManager($processors));
-			}
-
+			// Register the Phperclip service
 			return new PhperclipService(
 				$app->make('TippingCanoe\Phperclip\Repository\File'),
 				$app->make('TippingCanoe\Phperclip\Processes\ProcessManager'),
@@ -83,5 +61,57 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider{
 
 		});
 
+	}
+
+	private function registerAwsClient($config){
+		//
+		// Amazon S3
+		//
+		if ($s3Config = $config->get('phperclip::s3')) {
+			$this->app->bind('Aws\S3\S3Client', function ($app) use ($s3Config) {
+				return \Aws\S3\S3Client::factory($s3Config);
+			});
+		}
+	}
+
+	private function registerFileNameGenerator()
+	{
+		$this->app->bind('TippingCanoe\Phperclip\Contracts\FileNameGenerator', $this->app->make('config')->get('phperclip::filename_generator'));
+	}
+
+	private function configureStorageDrivers($app, $config)
+	{
+		// Run through and call each config option as a setter on the storage method.
+		$storageDrivers = [];
+		foreach ($config->get('phperclip::storage', []) as $abstract => $driverConfig) {
+
+			$driver = $app->make($abstract);
+
+			foreach ($driverConfig as $property => $value) {
+				$setter = studly_case('set_' . $property);
+				$driver->$setter($value);
+			}
+
+			$storageDrivers[$abstract] = $driver;
+		}
+
+		return $storageDrivers;
+	}
+
+	private function registerFileProcessors($app, $config)
+	{
+
+		if ($processorConfig = $config->get('phperclip::processors')) {
+
+			$processors = [];
+
+			foreach ($processorConfig as $processor) {
+				$processors[] = $app->make($processor);
+			}
+
+			$this->app->bindShared('TippingCanoe\Phperclip\Processes\ProcessManager', function () use ($processors) {
+				return new ProcessManager($processors);
+			});
+		}
 	}
 }
