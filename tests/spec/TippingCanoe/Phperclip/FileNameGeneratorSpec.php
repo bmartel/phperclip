99be1e4264bb0f5741a00ef18f437c5675dd99b7
bmartel <brandonmartel@gmail.com>
2014-11-05 22:25:27 -0600

    small test change for the default file name generator implementation

--- tests/spec/TippingCanoe/Phperclip/FileNameGeneratorSpec.php
+++ tests/spec/TippingCanoe/Phperclip/FileNameGeneratorSpec.php
@@ -26,7 +26,7 @@ class FileNameGeneratorSpec extends ObjectBehavior
 		$fileModel->setAttribute('slot', 'profile');
 
 		$options = [
-			$this->getFileModificationKey() => [
+			$this->getFileModificationKey()->willReturn('modifications') => [
 				'shrink'=>'run shrink filter here',
 				'blur' => 'run blur filter here'
 			],
@@ -41,7 +41,7 @@ class FileNameGeneratorSpec extends ObjectBehavior
 			'validators' => [
 				'file_size' => 'run file size validator'
 			],
-			$this->getFileModificationKey() => [
+			$this->getFileModificationKey()->willReturn('modifications') => [
 				'blur' => 'run blur filter here',
 				'shrink'=>'run shrink filter here'
 			]
