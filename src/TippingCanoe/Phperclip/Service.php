3fcbc45232d806b6b3b3b45d4c489afb2d4bb9cb
BrandonMartel <b.martel@tippingcanoe.com>
2015-01-12 11:38:04 -0600

    If an appropriate file is not returned from a processing call, halt further operations.

--- src/TippingCanoe/Phperclip/Service.php
+++ src/TippingCanoe/Phperclip/Service.php
@@ -175,7 +175,9 @@ class Service {
 		// and the current action scope.
 
 		// Run any pre-save file processing, such as validation
-		if (($file = $this->processManager->dispatch($file, 'onBeforeSave', $options)) == false) {
+		$file = $this->processManager->dispatch($file, 'onBeforeSave', $options);
+
+		if (($file instanceof File) === false) {
 			return null;
 		}
 
@@ -225,8 +227,9 @@ class Service {
 
 		// Determine if there are any registered processors which know of this file type
 		// and the current action scope.
+		$fileModel = $this->processManager->dispatch($fileModel, 'onDelete', $options);
 
-		if (($fileModel = $this->processManager->dispatch($fileModel, 'onDelete', $options)) == false) {
+		if (($fileModel instanceof FileModel) === false) {
 			return null;
 		}
 
@@ -318,8 +321,9 @@ class Service {
 
 		// Determine if there are any registered processors which know of this file type
 		// and the current action scope.
+		$fileModel = $this->processManager->dispatch($fileModel, 'onMove');
 
-		if (($fileModel = $this->processManager->dispatch($fileModel, 'onMove')) == false) {
+		if (($fileModel instanceof FileModel) === false) {
 			return null;
 		}
 
@@ -408,7 +412,9 @@ class Service {
 	protected function saveFile(File $file, FileModel $fileModel, array $options = []) {
 
 		// Run any file manipulation processors
-		if (($file = $this->processManager->dispatch($file, 'onSave', $options)) == false) {
+		$file = $this->processManager->dispatch($file, 'onSave', $options);
+
+		if (($file instanceof File) === false) {
 			// If something fails inside the processors, clean up the file instances
 			$this->delete($file, $options);
 			return null;
